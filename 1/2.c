e69aea36424bbb589163cc8f52818cf6eec8f59a
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-11-11 17:39:17 +0300

    Update 2.c

--- 1/2.c
+++ 1/2.c
@@ -8,32 +8,52 @@ int main()
 
 	int var;
 
-	printf(" 0. Воскресенье \n 1. Понедельник \n 2. Вторник \n 3. Среда \n 4. Четверг \n 5. Пятница \n 6. Суббота \n Введите цифру дня недели: ");
+	printf(" 0. Воскресенье \n 1. Понедельник \n 2. Вторник \n 3. Среда \n 4. Четверг \n 5. Пятница \n 6. Суббота \n");
 	do
 	{
-		scanf("%d", &var);
-	
+		printf("\nВведите цифру дня недели от 0 до 6: : ");
+		while (1)
+		{
+			int c = scanf("%d", &var);
+
+			if (c == 1)
+			{
+				if (getchar() != '\n')
+				{
+					printf("Некорректный ввод. Введите только одно число: ");
+					while (getchar() != '\n');
+				}
+				else
+				{
+					break;
+				}
+			}
+			else
+			{
+				printf("Некорректный ввод. Введите только одно число: ");
+				while (getchar() != '\n');
+			}
+		}
 		switch (var)
 		{
-		case 0: printf("Воскресенье");
+		case 0: printf("\nВоскресенье\n");
 			break;
-		case 1: printf("Понедельник");
+		case 1: printf("\nПонедельник\n");
 			break;
-		case 2: printf("Вторник");
+		case 2: printf("\nВторник\n");
 			break;
-		case 3: printf("Среда");
+		case 3: printf("\nСреда\n");
 			break;
-		case 4: printf("Четверг");
+		case 4: printf("\nЧетверг\n");
 			break;
-		case 5: printf("Пятница");
+		case 5: printf("\nПятница\n");
 			break;
-		case 6: printf("Суббота");
+		case 6: printf("\nСуббота\n");
 			break;
 		default:
-			printf("Некорректный ввод, введите цифру от 0 до 6: ");
-			break;
+			printf("Некорректный ввод");
 		}
-	}  while (var < 0 || var > 6);
+	} while (var < 0 || var > 6);
 
 	return 0;
 }
