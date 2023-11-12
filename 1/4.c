369d29910547ee23aade9bc84f0ae7a91dda30d2
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-11-12 18:50:19 +0300

    Update 4.c

--- 1/4.c
+++ 1/4.c
@@ -2,33 +2,49 @@
 #include <stdio.h>
 #include <math.h>
 #include <locale.h>
+#include <string.h>
 
 int main() {
 
     setlocale(LC_ALL, "Russian");
 
     int x, y;
+    char input[100];
     printf("Введите значение x: ");
     while (1)
     {
-        if (scanf("%i", &x) == 1)
+        while (1)
         {
-            if (getchar() != '\n')
+            if (scanf("%s", input) == 1)
             {
-                printf("Некорректный ввод.\nВведите только одно значение: ");
-                while (getchar() != '\n');
-            }
-            else
-            {
-                break;
+                if (strcmp(input, "exit") == 0)
+                {
+                    printf("\nВыход из программы\n");
+                    return 0;
+                }
+                else
+                {
+                    if (sscanf(input, "%i", &x) == 1)
+                    {
+                        if (getchar() != '\n')
+                        {
+                            printf("Некорректный ввод.\nВведите только одно значение: ");
+                            while (getchar() != '\n');
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        printf("Некорректный ввод.\nВведите без букв, только одно значение: ");
+                        while (getchar() != '\n');
+                    }
+                }
             }
         }
-        else
-        {
-            printf("Некорректный ввод.\nВведите без букв, только одно значение: ");
-            while (getchar() != '\n');
-        }
-    }
+
     if (x > 0)
     {
         y = 2 * x - 10;
@@ -41,6 +57,6 @@ int main() {
     {
         y = abs(x) - 1;
     }
-    printf("Значение функции при x(%i) = %i", x, y);
-    return 0;
+    printf("Значение функции при x(%i) = %i\nЧтобы выйти из программы напишите exit\nИли введите следущее значение x :", x, y);
+    }
 }
