c151c13d7d4b0416264d1a14a659f9ea3c24c834
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-11-12 15:26:32 +0300

    Update 2.c

--- 1/2.c
+++ 1/2.c
@@ -8,51 +8,42 @@ int main()
 
 	int var;
 
-	printf(" 0. Воскресенье \n 1. Понедельник \n 2. Вторник \n 3. Среда \n 4. Четверг \n 5. Пятница \n 6. Суббота \n Введите цифру дня недели от 0 до 6: ");
+	printf(" 0. Воскресенье \n 1. Понедельник \n 2. Вторник \n 3. Среда \n 4. Четверг \n 5. Пятница \n 6. Суббота \n");
 
 	while (1)
 	{
+		printf("Введите только одну цифру от 0 до 6: ");
 		if (scanf("%d", &var) == 1)
 		{
-			if (getchar() != '\n')
+			while (getchar() != '\n');
+			if (0 >= var <= 6)
 			{
-				printf("Некорректный ввод. Введите только одну цифру: ");
-				while (getchar() != '\n');
+				switch (var)
+				{
+				case 0: printf("\nВоскресенье\n");
+					return 0;
+				case 1: printf("\nПонедельник\n");
+					return 0;
+				case 2: printf("\nВторник\n");
+					return 0;
+				case 3: printf("\nСреда\n");
+					return 0;
+				case 4: printf("\nЧетверг\n");
+					return 0;
+				case 5: printf("\nПятница\n");
+					return 0;
+				case 6: printf("\nСуббота\n");
+					return 0;
+				}
 			}
 			else
 			{
-				if (0 >= var <= 6)
-				{
-					switch (var)
-					{
-					case 0: printf("\nВоскресенье\n");
-						return 0;
-					case 1: printf("\nПонедельник\n");
-						return 0;
-					case 2: printf("\nВторник\n");
-						return 0;
-					case 3: printf("\nСреда\n");
-						return 0;
-					case 4: printf("\nЧетверг\n");
-						return 0;
-					case 5: printf("\nПятница\n");
-						return 0;
-					case 6: printf("\nСуббота\n");
-						return 0;
-					default:
-						printf("Некорректный ввод. Введите цифру от 0 до 6.\n");
-						continue;
-					}
-				}
-				else
-				{
-					printf("Цифра должно быть от 0 до 6. Пожалуйста, введите корректную цифру.\n");
-				}
+				continue;
 			}
 		}
 		else
 		{
-			printf("Некорректный ввод. Введите только одну цифру: ");
+			printf("Некорректный ввод. Введите только одну цифру от 0 до 6: ");
 			while (getchar() != '\n');
 		}
 	}
