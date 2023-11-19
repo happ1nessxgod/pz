c4dfbe918e0e9e81254ab7f3d6c3bbdc9498781a
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-11-19 18:26:19 +0300

    Create 2 мой метод.c

---
+++ 3/2 мой метод.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+void NOD(int x, int y)
+{
+	int max = abs(fmax(x, y));
+	int min = abs(fmin(x, y));
+
+	if (max % min == 0) {
+		printf("Общий делитель: %i", min);
+	}
+	else
+	{
+		for (int i = min; i > 1; --i)
+			if (max % i == 0 && min % i == 0) {
+				printf("Общий делитель: %i", i);
+				break;
+		}
+		printf("Нет делителя");
+	}
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int x, y;
+	printf("Введите 2 числа: ");
+	scanf("%i %i", &x, &y);
+	NOD(x, y);
+	return 0;
+}
