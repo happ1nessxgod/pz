225a29f1dfb1964e0b5ed15c080ff6a0064da36e
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-11-22 02:08:11 +0300

    Update 6.c

--- 2/6.c
+++ 2/6.c
@@ -5,7 +5,7 @@ int main() {
     
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
-            if (i > j) {
+            if (i >= j) {
                 printf("%2d ", i * size + j); 
             } else {
                 printf("   ");
