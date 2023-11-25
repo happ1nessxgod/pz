d3a320191f4bad70bd20de8d732b4f6294c0e49d
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-11-25 04:12:06 +0300

    Create 41.c

---
+++ 5 указатели/41.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    float array[11] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10, 10.11};
+    float* ptr_a;
+    ptr_a = array;
+    for (int i = 0; i < 11; ++i) {
+        printf("Элемент %d: %f\n", i + 1, *ptr_a);
+        ptr_a++;
+    }
+    printf("%p %p %p", &ptr_a, array, &array[2]);
+
+    return 0;
+}
