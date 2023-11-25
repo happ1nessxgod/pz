e011f72df4f326a5ba91a1a4fc70ca3253203aa0
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-11-25 18:32:33 +0300

    Update 44.c

--- 5 указатели/43.c
+++ 5 указатели/43.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main() {
 
     float array[10] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10 };
@@ -8,6 +7,18 @@ int main() {
     for (; ptr_a < &array[10]; ++ptr_a) {
         printf("%f:\n", *ptr_a);
     }
-    printf("%p %p", ptr_a-10, &array[0]);
+    printf("%p %p", ptr_a - 10, &array[0]);
+    return 0;
+}
+
+#include <stdio.h>
+int main() {
+    float array[10] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10 };
+    float* ptr_a;
+    ptr_a = &array[9];
+    for (; ptr_a > &array[0]; --ptr_a) {
+        printf("%f:\n", *ptr_a);
+    }
+    printf("%p %p", ptr_a, &array[0]);
     return 0;
 }
