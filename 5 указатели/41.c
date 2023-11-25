7d4e1f5824a81720ed992162bef0281be2dd3b96
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-11-25 16:26:29 +0300

    Update 41.c

--- 5 указатели/41.c
+++ 5 указатели/41.c
@@ -3,10 +3,11 @@
 int main() {
 
     float array[10] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10 };
-    float* ptr_a, *ptr_b;
+    float* ptr_a, ptr_b;
     ptr_a = &array;
-    for (int i = 0; i < 10; ++i) {
-        printf("Элемент %d: %f\n", i + 1, *ptr_a);
+    ptr_b = (&array[10] - &array);
+    for (int i = 0; i < ptr_b; ++i) {
+        printf("%d: %f\n", i + 1, *ptr_a);
         ptr_a++;
     }
     printf("%p %p %p", &array, &array[0], array);
