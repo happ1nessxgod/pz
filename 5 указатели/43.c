ff24cc79702007afece3a20181bc14ab9e7d0d1f
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-11-25 16:05:12 +0300

    Update 43.c

--- 5 указатели/43.c
+++ 5 указатели/43.c
@@ -4,12 +4,10 @@ int main() {
 
     float array[10] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10 };
     float* ptr_a;
-    float* ptr_b;
-    ptr_a = &array[0];
-    ptr_b = &array[10];
-    for (; ptr_a < ptr_b ; ++ptr_a) {
-        printf("Элемент %f:\n", *ptr_a);
+    ptr_a = &array;
+    for (; ptr_a < &array[10]; ++ptr_a) {
+        printf("%f:\n", *ptr_a);
     }
-
+    printf("%p %p", ptr_a-10, &array[0]);
     return 0;
 }
