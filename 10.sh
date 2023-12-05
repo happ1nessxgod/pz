2be3515e9064e02b65721bb1071ce5b9aad4a381
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-12-05 18:04:52 +0300

    Update 10.sh

--- 10.sh
+++ 10.sh
@@ -21,3 +21,41 @@ for i in "${!array[@]}"; do
         fi
 done 
 echo "summa - $mu"
+
+
+
+#!/bin/bash
+array=( "$1" "$2" "$3" "$4" )
+min=${array[0]}
+mini=0
+mu=0
+
+for i in "${!array[@]}"; do
+    if ((array[i] < min)); then
+        min=${array[i]}
+        mini=$i
+    fi
+done
+
+echo "znachenie - $min"
+echo "element - array[$mini]"
+
+# Проверка первых двух элементов на отрицательность
+if ((array[0] < 0 && array[1] < 0)); then
+    mu=0
+else
+    # Поиск первого отрицательного элемента
+    for i in "${!array[@]}"; do
+        if (( "${array[i]}" < 0)); then
+            j=$((i + 1))
+            # Суммирование положительных элементов после второго отрицательного
+            while ((j < ${#array[@]} && array[j] >= 0)); do 
+                mu=$((mu + array[j]))
+                ((j++))
+            done
+            break
+        fi
+    done
+fi
+
+echo "summa - $mu"
