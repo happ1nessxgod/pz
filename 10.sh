af3add48ad89408f5d4083b1d791ca88187b7a1f
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-12-05 16:55:49 +0000

    Update 10.sh

--- 10.sh
+++ 10.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
-array=( "$1" "$2" "$3" "$4" )
-min=${array[0]}
+last=-1
+sum=0
+array=( "$1" "$2" "$3" "$4" "$5" )
 mini=0
-mu=0
 for i in "${!array[@]}"; do
-        if ((array[i] < min)); then
-                min=${array[i]}
-                mini=$i
-        fi
+	if [ "${array[i]}" -eq 0 ]; then
+		last=$i
+	fi
 done
-echo "znachenie - $min"
-echo "element - array[$mini]"
+
+if [ $last -eq -1 ]; then
+	echo "v massive net nuley"
+	exit 1
+fi
+
 for i in "${!array[@]}"; do
-        if (( "${array[i]}" < 0)); then
-                j=$((i + 1))
-                while ((j < ${#array[@]} && array[j] >= 0)); do 
-                        mu=$((mu + array[j]))
-                        ((j++))
-                done
-        fi
-done 
-echo "summa - $mu"
+    if ((array[i] > 0)); then
+        ((++mini))
+    fi
+done
+
+echo "kolvo - $mini"
+
+for ((i = last + 1; i < ${#array[@]}; i++)); do
+	sum=$((sum + array[i]))
+done
+
+echo "summa - $sum"
+
 
 
 
