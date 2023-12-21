d39193b4c527455fa64436528b61ba3bee2617b0
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-12-21 11:15:07 +0300

    Create 7.c

---
+++ 9/7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    int n = 4;
+    int m = 4;
+    int** a = (int**)malloc(n * sizeof(int*));
+
+    for (int i = 0; i < n; i++) {
+        a[i] = (int*)malloc(m * sizeof(int));
+    }
+
+    int something = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            a[i][j] = something;
+            something++;
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i][n - i - 1];
+    }
+    printf("сумма элементов главной диагонали: %d\n", sum);
+    for (int i = 0; i < n; i++) {
+        free(a[i]);
+    }
+    free(a);
+
+    return 0;
+}
