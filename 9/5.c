e2d51d9a13a6ffbd9e5cab08c67972555b35c4f2
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-12-21 11:09:52 +0300

    Create 5.c

---
+++ 9/5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n = 3;
+    int m = 4;
+    int** a = (int**)malloc(n * sizeof(int*));
+
+    for (int i = 0; i < n; i++) {
+        a[i] = (int*)malloc(m * sizeof(int));
+    }
+
+    int something = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            a[i][j] = something;
+            something++;
+        }
+    }
+
+    int max[4];
+    for (int j = 0; j < m; j++) {
+        max[j] = a[0][j];
+        for (int i = 1; i < n; i++) {
+            if (a[i][j] > max[j]) {
+                max[j] = a[i][j];
+            }
+        }
+    }
+
+    printf("максимальные значения в каждом столбце:\n");
+    for (int j = 0; j < m; j++) {
+        printf("Столбец %d: %d\n", j, max[j]);
+    }
+    for (int i = 0; i < n; i++) {
+        free(a[i]);
+    }
+    free(a);
+
+    return 0;
+}
