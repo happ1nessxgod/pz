1f58531a4fd7a81be3f1e07243b2454d195e2b53
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-12-21 03:04:37 +0300

    Update 3.c

--- 8/3.c
+++ 8/3.c
@@ -2,32 +2,47 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main() {
-    setlocale(LC_ALL, "RUS");
-    int i, j, count[9] = { 0 };
-    int ap[3][3];
-    for (i = 0; i < 3; ++i) {
-        for (j = 0; j < 3; ++j) {
-            printf("ap[%d][%d] = ", i, j);
-            scanf("%d", &ap[i][j]);
+void qwe(int num) {
+    int digits[10] = { 0 };
 
-            int index = i * 3 + j;
-            ++count[index];
+    if (num < 0) {
+        num = -num;
+    }
+
+    do {
+        int digit = num % 10;
+        digits[digit]++;
+        num /= 10;
+    } while (num > 0);
+
+    for (int i = 0; i < 10; i++) {
+        if (digits[i] > 1) {
+            printf("%d повторяется %d раз(а).\n", i, digits[i]);
         }
     }
-    printf("выберите элемент массива - \n\n");
-    for (int k = 0; k < 9; ++k) {
-        printf("%d - p[%d][%d]\n", k + 1, k / 3, k % 3);
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    int array[3][3];
+
+    printf("Введите элементы массива 3x3:\n");
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("array[%d][%d]: ", i, j);
+            scanf("%d", &array[i][j]);
+        }
     }
-    int kj;
-    scanf("%d", &kj);
-    if (kj >= 1 && kj <= 9) {
-        int row = (kj - 1) / 3;
-        int col = (kj - 1) % 3;
-        printf("колво знаков ""%d"" повторяется - %d раз", ap[row][col], count[kj - 1]);
-    }
-    else {
-        printf("неверный выбор");
+
+    printf("результат:\n");
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("В ячейке [%d][%d] число %d:\n", i, j, array[i][j]);
+            qwe(array[i][j]);
+            printf("\n");
+        }
     }
     return 0;
 }
