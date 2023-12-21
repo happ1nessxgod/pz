0b5f56b16273d5f4aee8af45a6ec8aeeb681c08a
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-12-21 05:13:47 +0300

    Update 10.c

--- 8/10.c
+++ 8/10.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <locale.h>
 
-void findMinSumColumns(int arr[][4], int rows, int cols, int* minCol1, int* minCol2) {
-    int minSum = -1;
+void findMinSumColumns(int arr[][4], int rows, int cols, int* minCol1, int* minCol2, int* minSum) {
+    *minSum = -1;
     for (int j = 0; j < cols - 1; ++j) {
         int currentSum = 0;
         for (int i = 0; i < rows; ++i) {
             currentSum += arr[i][j] + arr[i][j + 1];
         }
-        if (minSum == -1 || currentSum < minSum) {
-            minSum = currentSum;
+        if (*minSum == -1 || currentSum < minSum) {
+            *minSum = currentSum;
             *minCol1 = j;
             *minCol2 = j + 1;
         }
@@ -22,10 +22,11 @@ int main() {
     int arr[3][4] = {
         {1, 2, 3, 4},
         {5, 6, 7, 8},
-        {9, 10, 11, 12}
+        {-9, 10, -11, 12}
     };
-    int minCol1, minCol2;
-    findMinSumColumns(arr, rows, cols, &minCol1, &minCol2);
+    int minCol1, minCol2, minSum;
+    findMinSumColumns(arr, rows, cols, &minCol1, &minCol2, &minSum);
     printf("номера двух соседних столбцов с минимальной суммой элементов: %d и %d\n", minCol1, minCol2);
+    printf("минимальная сумма элементов: %d\n", minSum);
     return 0;
 }
