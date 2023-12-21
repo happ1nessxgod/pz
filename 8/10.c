954c9db2608265dbff2494573ed89f3659fdc403
happ1nessxgod <67000247+happ1nessxgod@users.noreply.github.com>
2023-12-21 15:45:04 +0300

    Update 10.c

--- 8/10.c
+++ 8/10.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #include <locale.h>
 
-void findMinSumColumns(int arr[][4], int rows, int cols, int* minCol1, int* minCol2, int* minSum) {
-    *minSum = -1;
+void qwe(int arr[][4], int rows, int cols, int* mincol1, int* mincol2, int* minsum) {
+    *minsum = -1;
     for (int j = 0; j < cols - 1; ++j) {
         int currentSum = 0;
         for (int i = 0; i < rows; ++i) {
             currentSum += arr[i][j] + arr[i][j + 1];
         }
-        if (*minSum == -1 || currentSum < minSum) {
-            *minSum = currentSum;
-            *minCol1 = j;
-            *minCol2 = j + 1;
+        if (*minsum == -1 || currentSum < *minsum) {
+            *minsum = currentSum;
+            *mincol1 = j;
+            *mincol2 = j + 1;
         }
     }
 }
+
 int main() {
     setlocale(LC_ALL, "RUS");
     const int rows = 3;
     const int cols = 4;
     int arr[3][4] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {-9, 10, -11, 12}
+        {1, 2, -3, 4},
+        {-5, 6, 7, 8},
+        {-9, -10, 11, 12}
     };
-    int minCol1, minCol2, minSum;
-    findMinSumColumns(arr, rows, cols, &minCol1, &minCol2, &minSum);
-    printf("номера двух соседних столбцов с минимальной суммой элементов: %d и %d\n", minCol1, minCol2);
+    int mincol1, mincol2, minsum;
+    qwe(arr, rows, cols, &mincol1, &mincol2, &minSum);
+
+    minSum = 0;
+    for (int i = 0; i < rows; ++i) {
+        minSum += arr[i][mincol1] + arr[i][mincol2];
+    }
+    printf("номера двух соседних столбцов с минимальной суммой элементов: %d и %d\n", mincol1+1, mincol2+1);
     printf("минимальная сумма элементов: %d\n", minSum);
     return 0;
 }
